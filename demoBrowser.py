99e7625adbe740d9e5bb0a51d4d3438c711a229e
Mohammad.Faiz <faiz999888777@gmail.com>
2024-02-08 14:57:41 +0530

    chromedriver

--- demoBrowser.py
+++ demoBrowser.py
@@ -2,6 +2,12 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 
 #chrome driver without local chromedriver
-service_obj = Service()
-driver = webdriver.Chrome(service=service_obj)
+# service_obj = Service()
+# driver = webdriver.Chrome(service=service_obj)
+
+
+driver = webdriver.Chrome(executable_path="C:/Users/faiz/Downloads/Udemy_Selenium-Practice/Selenium_Course/chromedriver-win64/chromedriver.exe")
+
 driver.get("https://www.linkedin.com/feed/")
+
+driver.close()
