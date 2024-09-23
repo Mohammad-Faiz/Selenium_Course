b8f6b0a01ae564cd349aaf4d2021e514aebb4dc7
Mohammad Faiz <mohammadfaiz1410@gmail.com>
2024-09-23 22:58:05 +0530

    Update fixture.py

--- fixture.py
+++ fixture.py
@@ -1,11 +1,34 @@
 Fixture
 
-@pytest.fixture()
 
+@pytest.fixture()
 def setup():
   print("This will execute first")
-  yield #this will execute at last 
-  ("This will execute at last")
+  yield 
+  print("This will execute at last")
+"""ABOVE FUNCTION PUT IN ANOTHER PAGE"""
+
+
+
 
 def test_fixturedemo():
   print("This will execute after setup")
+
+def test_fixturedemo1():
+  print("This will execute after setup2")
+  
+def test_fixturedemo2():
+  print("This will execute after setup3")
+
+FOR ABOVE CODE WE WILL GET OUTPUT: - 
+This will execute first
+This will execute after setup
+This will execute at last
+
+
+IF WE CHANGE 
+def setup(scope = "class"):
+  print("This will execute first")
+  yield 
+  print("This will execute at last")
+#now at the beggining of test test setup will run and at last of the all test yield will run
